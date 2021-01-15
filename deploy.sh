6ad8562bc7eedaea584a14c2e2948dbdf3710886
GraceYu <librayuyueran@gmail.com>
2021-01-15 16:27:26 -0600

    update branch deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf src *.html *.css *.sh *.png *.js *.json yarn.lock &&
-mv dist/* ./ &&
-rm -rf dist;
-git add . &&
-git commit -m "update branch deploy" &&
-git push
