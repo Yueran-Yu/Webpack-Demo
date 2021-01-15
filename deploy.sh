76dd68d26331e9a37078246667ec6de6e842c657
GraceYu <librayuyueran@gmail.com>
2021-01-15 16:01:25 -0600

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+yarn build &&
+git checkout gh-pages &&
+rm -rf src *.html *.css *.sh *.png *.js *.json yarn.lock &&
+mv dist/* ./ &&
+rm -rf dist;
+git add . &&
+git commit -m "update branch deploy" &&
+git push
